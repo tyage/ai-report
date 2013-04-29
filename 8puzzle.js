9c06afaa46304c630261c4215a8e4566e0cccb56
tyage <namatyage@gmail.com>
2013-04-29 12:20:07 +0900

    8puzzle

---
+++ 8puzzle.js
@@ -0,0 +1,222 @@
+(function() {
+var Puzzle = function(pieces, distance) {
+	this.pieces = pieces;
+	this.distance = distance;
+	this.children = [];
+	this.parent = null;
+};
+Puzzle.SPACE = 9;
+Puzzle.WIDTH = 3;
+Puzzle.HEIGHT = 3;
+Puzzle.prototype = {
+	f: function() {
+		return this.g() + this.h2();
+	},
+	g: function() {
+		return this.distance;
+	},
+	// 位置が正しくないタイルの数
+	h1: function() {
+		var fail = 0;
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				var correct = i * Puzzle.WIDTH + j + 1;
+				(this.pieces[i][j] !== correct && this.pieces[i][j] !== Puzzle.SPACE) && ++fail;
+			}
+		}
+		return fail;
+	},
+	// マンハッタン距離
+	h2: function() {
+		var distance = 0;
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				var piece = this.pieces[i][j],
+					correct = i * Puzzle.WIDTH + j + 1,
+					currentPos = {
+						x: (correct-1) % 3,
+						y: parseInt(correct / 3)
+					},
+					correctPos = {
+						x: (piece-1) % 3,
+						y: parseInt(piece / 3)
+					};
+				distance += Math.sqrt(Math.pow(correctPos.x-currentPos.x, 2) + Math.pow(correctPos.y-currentPos.y, 2));
+			}
+		}
+		return distance;
+	},
+	move: function(direction) {
+		var prePos = this.getSpacePos(),
+			nextPos = {
+				x: prePos.x + direction.x,
+				y: prePos.y + direction.y
+			},
+			nextPieces = [];
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			nextPieces[i] = [];
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				nextPieces[i][j] = this.pieces[i][j];
+			}
+		}
+
+		if (nextPos.x < 0 || nextPos.x >= Puzzle.WIDTH || nextPos.y < 0 || nextPos.y >= Puzzle.HEIGHT) {
+			return false;
+		}
+
+		nextPieces[prePos.y][prePos.x] = this.pieces[nextPos.y][nextPos.x];
+		nextPieces[nextPos.y][nextPos.x] = this.pieces[prePos.y][prePos.x];
+		return nextPieces;
+	},
+	getSpacePos: function() {
+		var pos = {};
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				if (this.pieces[i][j] === Puzzle.SPACE) {
+					pos = {
+						x: j,
+						y: i
+					};
+				}
+			}
+		}
+		return pos;
+	},
+	isFinished: function() {
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				var correct = i * Puzzle.WIDTH + j + 1;
+				if (this.pieces[i][j] !== correct && this.pieces[i][j] !== Puzzle.SPACE) {
+					return false;
+				}
+			}
+		}
+		return true;
+	},
+	matchPieces: function(pieces) {
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				if (this.pieces[i][j] !== pieces[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	},
+	draw: function(ctx, pos) {
+		var x = pos * 90 + 20,
+			y = this.distance * 80,
+			width = 60,
+			height = 60;
+		this.center = {
+			x: x + width / 2,
+			y: y + height / 2
+		};
+
+		ctx.beginPath();
+		ctx.strokeRect(x, y, width, height);
+
+		ctx.moveTo(x + width / 3, y);
+		ctx.lineTo(x + width / 3, y + height);
+		ctx.stroke();
+		ctx.moveTo(x + width * 2 / 3, y);
+		ctx.lineTo(x + width * 2 / 3, y + height);
+		ctx.stroke();
+		ctx.moveTo(x, y + height / 3);
+		ctx.lineTo(x + width, y + height / 3);
+		ctx.stroke();
+		ctx.moveTo(x, y + height * 2 / 3);
+		ctx.lineTo(x + width, y + height * 2 / 3);
+		ctx.stroke();
+
+		for (var i=0;i<Puzzle.HEIGHT;++i) {
+			for (var j=0;j<Puzzle.WIDTH;++j) {
+				var text = this.pieces[i][j] === Puzzle.SPACE ? '' : this.pieces[i][j];
+				ctx.fillText(text, x + width * (j+0.3) / Puzzle.WIDTH, y + height * (i+0.7) / Puzzle.HEIGHT);
+			}
+		}
+
+		ctx.fillText('('+parseInt(this.f())+')', x - 20, y + 10);
+
+		if (this.parent) {
+			ctx.moveTo(this.center.x, this.center.y - width / 2);
+			ctx.lineTo(this.parent.center.x, this.parent.center.y + width / 2);
+			ctx.stroke();
+		}
+
+		if (this.isFinished()) {
+			ctx.fillText('GOAL', x, y + height + 15);
+		}
+	}
+};
+
+var open = [],
+	closed = [];
+
+var step = function() {
+	if (open.length === 0) {
+		return true;
+	}
+
+	open.sort(function(a, b) {
+		return a.f() > b.f();
+	});
+	var s = open.shift();
+	closed.push(s);
+
+	for (var i=0;i<4;++i) {
+		var pieces = s.move({
+			x: [-1, 0, 1, 0][i],
+			y: [0, -1, 0, 1][i]
+		});
+		if (pieces && (!s.parent || !s.parent.matchPieces(pieces))) {
+			var child = new Puzzle(pieces, s.distance + 1);
+			child.parent = s;
+			s.children.push(child);
+			open.push(child);
+			if (child.isFinished()) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+};
+var draw = function() {
+	var nexts = [first],
+		currents,
+		ctx = document.getElementById('result').getContext('2d');
+	ctx.clearRect(document.getElementById('result').width, document.getElementById('result').height);
+	while (nexts.length > 0) {
+		currents = Array.apply(null, nexts);
+		nexts = [];
+
+		var pos = 0,
+			current;
+		while (current = currents.shift()) {
+			current.draw(ctx, pos);
+			++pos;
+
+			for (var i=0,l=current.children.length;i<l;++i) {
+				nexts.push(current.children[i]);
+			}
+		}
+	}
+};
+
+// here is the first pieces
+var first = new Puzzle([[4,1,2],[Puzzle.SPACE,6,3],[7,5,8]], 0);
+open.push(first);
+/*
+var timer = setInterval(function() {
+	if (step()) {
+		clearInterval(timer);
+	}
+	draw();
+}, 1000);
+*/
+while (!step()) {}
+
+// draw how to solve the puzzle
+window.addEventListener('load', draw);
+})();
